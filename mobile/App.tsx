3a77cd2ad649f0a96f3958c34be76264b11bc315
matheus <freitas1matheus@gmail.com>
2023-01-20 23:00:46 -0300

    aula 03 e 04

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,8 @@
 import './src/lib/dayjs';
+import './lib/dayjs';
 
 import { StatusBar } from 'react-native';
-import {
+import { 
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
@@ -10,7 +11,7 @@ import {
 } from '@expo-google-fonts/inter';
 
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +29,7 @@ export default function App() {
 
   return (
     <>
-      <Home />
+      <Routes />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </>
   );
